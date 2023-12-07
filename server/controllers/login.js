f7919325dbee462d153cddcb36a53fd6abda423b
Subhadeep <subhadeepsarkar1995@gmail.com>
2023-12-07 19:12:56 +0530

    added server

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt")
-const User = require("../models/User")
 const jwt = require("jsonwebtoken")
+const User = require("../models/User")
 require("dotenv").config()
 
 const login = async (req, res) => {
